b2f4bed0ad6ecf8f2e434b7bc857ab9352dfa449
Angela Palaszewski <delarosa.ang@gmail.com>
2023-09-23 13:27:27 -0600

    select working

--- components/ui/Select.jsx
+++ components/ui/Select.jsx
@@ -1,13 +1,16 @@
+"use client";
 import { Select, SelectItem, SelectSection } from "@nextui-org/select";
 
 export default function Services() {
   return (
-    <Select
-      label="Favorite Animal"
-      placeholder="Select an animal"
-      className="max-w-xs"
-    >
-      <SelectItem key="animal">animal</SelectItem>
+    <Select label="Choose a service">
+      <SelectItem>
+        <p>Boarding</p>
+        <p>For overnight care</p>
+      </SelectItem>
+      <SelectItem>Day Care</SelectItem>
+      <SelectItem>Drop In Visit</SelectItem>
+      <SelectItem>Walk</SelectItem>
     </Select>
   );
 }
